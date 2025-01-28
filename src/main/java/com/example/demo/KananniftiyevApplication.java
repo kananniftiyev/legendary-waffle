031552145f3863f79aae3ba12849633fb1df9e7f
kananniftiyev <kananniftiyev010@gmail.com>
2025-01-28 14:40:14 +0100

    init

---
+++ src/main/java/com/example/demo/KananniftiyevApplication.java
@@ -0,0 +1,19 @@
+/**
+ * @Author: Kanan Niftiyev
+ * @Date:   12::51::44 28-01-2025
+ * @Last Modified by:   Kanan Niftiyev
+ * @Last Modified time: 13::56::48 28-01-2025
+ */
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KananniftiyevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KananniftiyevApplication.class, args);
+	}
+
+}
